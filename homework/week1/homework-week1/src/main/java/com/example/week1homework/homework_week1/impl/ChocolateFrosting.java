4c82ba4c741b2ab65f11b1ac73feff5334dbdc15
harshvaghanii <harshvaghani98@gmail.com>
2025-11-24 21:50:43 -0600

    Adding loose coupling between frosting and syrup

---
+++ homework/week1/homework-week1/src/main/java/com/example/week1homework/homework_week1/impl/ChocolateFrosting.java
@@ -0,0 +1,15 @@
+package com.example.week1homework.homework_week1.impl;
+
+import com.example.week1homework.homework_week1.Frosting;
+import com.example.week1homework.homework_week1.FrostingType;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConditionalOnProperty(name = "cake.frosting", havingValue = "chocolate")
+public class ChocolateFrosting implements Frosting {
+    @Override
+    public FrostingType getFrostingType() {
+        return FrostingType.CHOCOLATE;
+    }
+}
---
+++ homework/week1/homework-week1/src/main/java/com/example/week1homework/homework_week1/impl/ChocolateSyrup.java
@@ -0,0 +1,15 @@
+package com.example.week1homework.homework_week1.impl;
+
+import com.example.week1homework.homework_week1.Syrup;
+import com.example.week1homework.homework_week1.SyrupType;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConditionalOnProperty(name = "cake.syrup", havingValue = "chocolate")
+public class ChocolateSyrup implements Syrup {
+    @Override
+    public SyrupType getSyrupType() {
+        return SyrupType.CHOCOLATE;
+    }
+}

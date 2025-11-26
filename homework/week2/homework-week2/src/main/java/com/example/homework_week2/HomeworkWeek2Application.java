446851e35ea761aa30554c95cc0a677dd4769815
harshvaghanii <harshvaghani98@gmail.com>
2025-11-25 23:53:08 -0600

    Initial commit for PR

--- homework/week2/homework-week2/src/main/java/com/example/homework_week2/HomeworkWeek2Application.java
+++ homework/week2/homework-week2/src/main/java/com/example/homework_week2/HomeworkWeek2Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HomeworkWeek2Application {
-
+// Adding a comment
 	public static void main(String[] args) {
 		SpringApplication.run(HomeworkWeek2Application.class, args);
 	}

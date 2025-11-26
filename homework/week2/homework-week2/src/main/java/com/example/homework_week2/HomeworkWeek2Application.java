ab466fd80a60268f1a6440c5fd03a4f86f631ce8
harshvaghanii <harshvaghani98@gmail.com>
2025-11-25 23:53:27 -0600

    Initial commit

--- homework/week2/homework-week2/src/main/java/com/example/homework_week2/HomeworkWeek2Application.java
+++ homework/week2/homework-week2/src/main/java/com/example/homework_week2/HomeworkWeek2Application.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HomeworkWeek2Application {
-// Adding a comment
-	public static void main(String[] args) {
-		SpringApplication.run(HomeworkWeek2Application.class, args);
-	}
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeworkWeek2Application.class, args);
+    }
 
 }

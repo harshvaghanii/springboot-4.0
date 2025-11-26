85843b3dbf998a47b154aa08239356436b5e7b1e
harshvaghanii <harshvaghani98@gmail.com>
2025-11-25 23:51:38 -0600

    initial commit for homework week 2

---
+++ homework/week2/homework-week2/src/test/java/com/example/homework_week2/HomeworkWeek2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homework_week2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeworkWeek2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

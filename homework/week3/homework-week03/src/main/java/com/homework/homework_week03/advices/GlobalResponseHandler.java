530c4c38824cb476d6733b191f8a0afabdafa973
harshvaghanii <harshvaghani98@gmail.com>
2025-11-30 19:58:58 -0600

    Adding starter code with db details

---
+++ homework/week3/homework-week03/src/main/java/com/homework/homework_week03/advices/GlobalResponseHandler.java
@@ -0,0 +1,21 @@
+package com.homework.homework_week03.advices;
+
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+@RestControllerAdvice
+public class GlobalResponseHandler implements ResponseBodyAdvice {
+    @Override
+    public boolean supports(MethodParameter returnType, Class converterType) {
+        return true;
+    }
+
+    @Override
+    public Object beforeBodyWrite(Object body, MethodParameter returnType, MediaType selectedContentType, Class selectedConverterType, ServerHttpRequest request, ServerHttpResponse response) {
+        return body instanceof ApiResponse<?> ? body : new ApiResponse(body);
+    }
+}

0a0eee90032fe7b9ae9888a75015702acf8d7854
harshvaghanii <harshvaghani98@gmail.com>
2025-11-30 19:22:46 -0600

    Initial commit

---
+++ homework/week3/homework-week03/src/main/java/com/homework/homework_week03/HomeworkWeek03Application.java
@@ -0,0 +1,13 @@
+package com.homework.homework_week03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkWeek03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeworkWeek03Application.class, args);
+	}
+
+}
---
+++ homework/week3/homework-week03/src/test/java/com/homework/homework_week03/HomeworkWeek03ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homework.homework_week03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeworkWeek03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

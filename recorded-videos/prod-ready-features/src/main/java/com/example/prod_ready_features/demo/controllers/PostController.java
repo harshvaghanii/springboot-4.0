e2c3fb7e000c81bdf0ee14094ef0b3bf3fb3b423
harshvaghanii <harshvaghani98@gmail.com>
2026-01-07 13:47:38 -0500

    Refactoring code

--- recorded-videos/prod-ready-features/src/main/java/com/example/prod_ready_features/demo/controllers/PostController.java
+++ recorded-videos/prod-ready-features/src/main/java/com/example/prod_ready_features/demo/controllers/PostController.java
@@ -7,7 +7,6 @@ import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestClient;
-import org.springframework.web.client.RestClientException;
 
 import java.util.List;
 
